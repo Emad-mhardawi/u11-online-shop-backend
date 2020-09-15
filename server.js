672dc1fab618d45e25554c08287c24336e4a92df
Emad Mhardawi <emad.mhardawi@chasacademy.se>
2020-09-15 21:20:14 +0200

    connect to mongoDb

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-const express = require('express');
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+require('dotenv').config()
 
 const app = express();
 
-app.use((req, res, next)=>{
-    res.send('hello from node ')
-})
+app.use((req, res, next) => {
+  res.send("hello from node ");
+});
+ 
 
-app.listen(process.env.port || 5000, ()=>{
-    console.log('server listening on port 5000')
+
+const PORT = process.env.port || 5000;
+
+app.listen(PORT, async () => {
+  console.log("server listening on port " + PORT);
+  try {
+    db = await mongoose.connect(process.env.mongoDB_connection_string, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("connected to the  database");
+  } catch (err) {
+    throw err;
+  }
 });
